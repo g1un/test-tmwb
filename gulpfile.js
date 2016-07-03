082fa8bad33b0dc7eb69848406a7d7539516d52d
I.Smal <serbov@pochta.ru>
2016-07-03 19:02:52 +0500

    initial

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var jade = require('gulp-jade');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var spritesmith = require('gulp.spritesmith');
+var merge = require('merge-stream');
+var replace = require('gulp-replace');
+
+gulp.task('sass', function(){
+	return gulp.src('scss/style.scss')
+	.pipe(sass({
+		outputStyle: 'expanded'
+	}))
+	.pipe(autoprefixer())
+	.pipe(replace('sprite.png', 'images/sprite.png'))
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.reload({
+		stream: true
+	}))
+});
+
+gulp.task('jade', function(){
+	return gulp.src('*.jade')
+			.pipe(jade({
+				pretty: true
+			}))
+			.pipe(gulp.dest('app'))
+			.pipe(browserSync.reload({
+				stream: true
+			}))
+});
+
+gulp.task('sprite', function () {
+	var spriteData = gulp.src('app/images/sprite/*.png').pipe(spritesmith({
+		imgName: 'sprite.png',
+		cssName: 'sprite.scss'
+	}));
+
+	var imgStream = spriteData.img
+			.pipe(gulp.dest('app/images/'));
+
+	var cssStream = spriteData.css
+			.pipe(gulp.dest('scss/components'));
+
+	return merge(imgStream, cssStream);
+});
+
+gulp.task('watch', ['browserSync', 'sass', 'jade', 'sprite'], function(){
+	gulp.watch('**/*.scss', ['sass']);
+	gulp.watch('*.jade', ['jade']);
+	gulp.watch('app/images/sprite/*.png', ['sprite']);
+	gulp.watch('app/*.html', browserSync.reload);
+	gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+gulp.task('browserSync', function() {
+	browserSync.init({
+		server: {
+			baseDir: 'app',
+			index: "index.html"
+		},
+	})
+});
